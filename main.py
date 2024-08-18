1d5ecf9fed2bc77a2a08b8a84f3393375a9bf695
devshazam <devshazam@gmail.com>
2024-08-18 14:37:27 +0300

    chore: Update README.md and requirements.txt

--- main.py
+++ main.py
@@ -2,64 +2,122 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
+from stockstats import StockDataFrame as sdf
+# import pmdarima as pm
 
-from config import *
 
 
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 # CONFIG:
-btc= yf.Ticker(TICKER_NAME)
-btc_usd_historical = btc.history(period=PERIOD_VAL, interval=INTERVAL_VAL)
-
+# period = “1d”, “5d”, “1mo”, “3mo”, “6mo”, “1y”, “2y”, “5y”, “10y”, “ytd”, “max”
+# interval = 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
+btc= yf.Ticker("BTC-USD")
+btc_usd_90Days = btc.history(period="3mo", interval="1d")
+btc_usd_30Days = btc.history(period="1mo", interval="1d")
+# print(btc_usd_90Days)
+# print(btc_usd_30Days)
 
 
 ### №1 - rolling window (RW)
-rol_RW = btc_usd_historical['Close'].mean()
-print('Стратегия №1', 'купить' if btc_usd_historical['Close'].iloc[-1] > rol_RW else 'продать')
+def rolling_window():
+    rol_RW = btc_usd_90Days['Close'].rolling(window=90, center=False).mean()
+    print('Стратегия №1', 'купить' if btc_usd_90Days['Close'].iloc[-1] > rol_RW.iloc[-1] else 'продать')
 
 ### №2 - simple moving average strategy (SMA)
-rol_SMA_1 = btc_usd_historical['Close'].rolling(window=15, center=False).mean()
-rol_SMA_2 = btc_usd_historical['Close'].rolling(window=30, center=False).mean()
-print('Стратегия №2', 'купить' if rol_SMA_1.iloc[-1] > rol_SMA_2.iloc[-1] else 'продать')
+def simple_moving_average():
+    rol_SMA_1 = btc_usd_90Days['Close'].rolling(window=45, center=False).mean()
+    rol_SMA_2 = btc_usd_90Days['Close'].rolling(window=90, center=False).mean()
+    print('Стратегия №2', 'купить' if rol_SMA_1.iloc[-1] > rol_SMA_2.iloc[-1] else 'продать')
 
 ### №3 - exponentially weighted moving average strategy (EWMA)
-rol_EWMA_1 = btc_usd_historical['Close'].ewm(span=15, adjust=False).mean()
-rol_EWMA_2 = btc_usd_historical['Close'].ewm(span=30, adjust=False).mean()
-print('Стратегия №3', 'купить' if rol_EWMA_1.iloc[-1] > rol_EWMA_2.iloc[-1] else 'продать')
+def exponentially_weighted_moving_average():
+    rol_EWMA_1 = btc_usd_90Days['Close'].ewm(span=5, adjust=True, ignore_na=True).mean()
+    rol_EWMA_2 = btc_usd_90Days['Close'].ewm(span=30, adjust=True, ignore_na=True).mean()
+    print('Стратегия №3', 'купить' if rol_EWMA_1.iloc[-1] > rol_EWMA_2.iloc[-1] else 'продать')
 
 ### №4 - RSI strategy
-rol_RSI = btc_usd_historical['Close'].ewm(span=14, adjust=False).mean()
-print('Стратегия №4', 'купить' if rol_RSI.iloc[-1] > btc_usd_historical['Close'].iloc[-1] else 'продать')
+def relative_strength_index():
+    stock = sdf.retype(btc_usd_30Days)
+    rol_RSI = stock['rsi_14'] # в книге rsi_12
+    rol_RSI_result = 'ничего'
+    if rol_RSI.iloc[-1] > 70: # в книге > 90
+        rol_RSI_result = 'продать'
+    elif rol_RSI.iloc[-1] < 30: # в книге < 10
+        rol_RSI_result = 'купить'
+    print('Стратегия №4', rol_RSI_result)
 
 ### №5 - MACD strategy
-rol_MACD = btc_usd_historical['Close'].ewm(span=12, adjust=False).mean() - btc_usd_historical['Close'].ewm(span=26, adjust=False).mean()
-rol_MACD_signal = rol_MACD.ewm(span=9, adjust=False).mean()
-print('Стратегия №5', 'купить' if rol_MACD.iloc[-1] > rol_MACD_signal.iloc[-1] else 'продать')
+def moving_average_convergence_divergence():
+    stock = sdf.retype(btc_usd_30Days)
+    signal = stock['macds']
+    macd = stock['macd']
+    rol_MACD_result = 'ничего'
+    if macd.iloc[-1] > signal.iloc[-1] and macd.iloc[-2] <= signal.iloc[-2]:
+        rol_MACD_result = 'купить'
+    elif macd.iloc[-1] < signal.iloc[-1] and macd.iloc[-2] >= signal.iloc[-2]:
+        rol_MACD_result = 'продать'
+    print('Стратегия №5', rol_MACD_result)
 
 ### №6 - RSI and MACD strategy
-rol_RSI = btc_usd_historical['Close'].ewm(span=14, adjust=False).mean()
-rol_MACD = btc_usd_historical['Close'].ewm(span=12, adjust=False).mean() - btc_usd_historical['Close'].ewm(span=26, adjust=False).mean()
-rol_MACD_signal = rol_MACD.ewm(span=9, adjust=False).mean()
-print('Стратегия №6', 'купить' if rol_RSI.iloc[-1] > rol_MACD_signal.iloc[-1] else 'продать')
+def rsi_and_macd():
+    stock = sdf.retype(btc_usd_30Days)
+    rsi = stock['rsi_12']
+    signal = stock['macds']
+    macd = stock['macd']
+    rol_RSI_MACD_result = 'ничего'
+    if rsi.iloc[-1] < 50 and macd.iloc[-1] > signal.iloc[-1] and macd.iloc[-2] <= signal.iloc[-2]:
+        rol_RSI_MACD_result = 'купить'
+    elif rsi.iloc[-1] > 50 and macd.iloc[-1] < signal.iloc[-1] and macd.iloc[-2] >= signal.iloc[-2]:
+        rol_RSI_MACD_result = 'продать'
+    print('Стратегия №6', rol_RSI_MACD_result)
+
 
 ### №7 - Triple exponential average strategy
-rol_TEMA = btc_usd_historical['Close'].ewm(span=9, adjust=False).mean() - btc_usd_historical['Close'].ewm(span=18, adjust=False).mean() + btc_usd_historical['Close'].ewm(span=36, adjust=False).mean()
+def triple_exponential_average():
+    stock = sdf.retype(btc_usd_30Days)
+    rol_TRIX = stock['trix_15']
+    rol_TRIX_result = 'ничего'
+    if rol_TRIX.iloc[-1] > 0 and rol_TRIX.iloc[-2] < 0:
+        rol_TRIX_result = 'продать'
+    elif rol_TRIX.iloc[-1] < 0 and rol_TRIX.iloc[-2] > 0:
+        rol_TRIX_result = 'купить'
+    print('Стратегия №7', rol_TRIX_result)
+
 
 ### №8 - Williams %R strategy
-rol_Williams = (btc_usd_historical['High'] - btc_usd_historical['Close']) / (btc_usd_historical['High'] - btc_usd_historical['Low']) * 100
-print('Стратегия №8', 'купить' if rol_Williams.iloc[-1] > 0 else 'продать') 
+def williams_percent_r():
+    stock = sdf.retype(btc_usd_30Days)
+    rol_Williams = stock.get('wr_6')
+    rol_Williams_result = 'ничего'
+    if rol_Williams.iloc[-1] < 10:
+        rol_Williams_result = 'продать'
+    elif rol_Williams.iloc[-1] > 90:
+        rol_Williams_result = 'купить'
+    print('Стратегия №8', rol_Williams_result)
 
 ########## Learning mean-reversion strategy ####################
 
 ### №9 - bollinger bands strategy
-btc_usd_historical['Middle Band'] = btc_usd_historical['Close'].rolling(window=20, center=False).mean()
-btc_usd_historical['Upper Band'] = btc_usd_historical['Middle Band'] + btc_usd_historical['Close'].rolling(window=20, center=False).std() * 2
-btc_usd_historical['Lower Band'] = btc_usd_historical['Middle Band'] - btc_usd_historical['Close'].rolling(window=20, center=False).std() * 2
-print('Стратегия №9', 'купить' if btc_usd_historical['Close'].iloc[-1] > btc_usd_historical['Upper Band'].iloc[-1] else 'продать')
+def bollinger_bands():
+    # print(btc_usd_30Days['Close'])
+    middle_base_line = btc_usd_30Days['Close'].mean()
+    std_line = btc_usd_30Days['Close'].std()
+    upper_band = middle_base_line + std_line * 2
+    lower_band = middle_base_line - std_line * 2
+
+    rol_BB_result = 'ничего'
+    if btc_usd_30Days['Close'].iloc[-1] < lower_band:
+        rol_BB_result = 'купить'
+    elif btc_usd_30Days['Close'].iloc[-1] > upper_band:
+        rol_BB_result = 'продать'
+    print('Стратегия №9', rol_BB_result)
+
+
 
 ### #10 - pairs trading strategy
-print('Стратегия №10', 'купить' if btc_usd_historical['Close'].iloc[-1] > btc_usd_historical['Open'].iloc[-1] else 'продать')
-
 
 ########## Learning mathematical model-based strategies ####################
 
@@ -70,12 +128,49 @@ print('Стратегия №10', 'купить' if btc_usd_historical['Close'].
 ########## Learning time-series prediction-based strategies ####################
 
 ### №13 - SARIMAX strategy
+def sarimax():
+    try:
+        model = pm.auto_arima(btc_usd_90Days['Close'], seasonal=True)
+        forecast = model.predict(7)
+        rol_SARIMAX_result = 'ничего'
+        if btc_usd_90Days['Close'].iloc[-1] > forecast[-1]:
+            rol_SARIMAX_result = 'продать'
+        else:
+            rol_SARIMAX_result = 'купить'
+        print('Стратегия №13', rol_SARIMAX_result)
+    except:
+        print('Ошибка в стратегии №13')
 
 ### #14 - Prophet strategy
+def prophet():
+    price_df = pd.DataFrame({'y': btc_usd_90Days['Close']}).rename_axis('ds').reset_index()
+    price_df['ds'] = price_df['ds'].dt.tz_convert(None)
 
+    model = pm.Prophet()
+    model.fit(price_df)
+    forecast = model.make_future_dataframe(periods=7, freq='D')
+    df_forecast = model.predict(df_forecast)
 
-# df = pd.read_csv('data.csv')
+    last_price = btc_usd_90Days['Close'].iloc[-1]
+    forecast_lower = df_forecast['yhat_lower'].iloc[-1]
+    forecast_upper = df_forecast['yhat_upper'].iloc[-1]
 
-# df.plot()
+    
+    rol_PROPHET_result = 'ничего'
+    if last_price < forecast_lower:
+        rol_PROPHET_result = 'купить'
+    elif last_price > forecast_upper:
+        rol_PROPHET_result = 'продать'
+    print('Стратегия №9', rol_PROPHET_result)
 
-# plt.show()
+rolling_window()
+simple_moving_average()
+exponentially_weighted_moving_average()
+bollinger_bands()
+relative_strength_index()
+moving_average_convergence_divergence()
+rsi_and_macd()
+triple_exponential_average()
+williams_percent_r()
+# sarimax()
+# prophet()
